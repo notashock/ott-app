a49be3af8a9f04280a351e2e9eb7fe4f83e5f35c
Ashok Babu Bavireddy <130152937+notashock@users.noreply.github.com>
2024-12-01 17:01:11 +0530

    Modified

--- src/unused_comp/MovDetail.jsx
+++ src/unused_comp/MovDetail.jsx
@@ -1,17 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const MovDetail = () => {
-  return (
-    <div className="mov-detail">
-        <div className="Mov-card">
-            <div className="title-card">
-                <h1></h1>
-                <h3></h3>
-            </div>
-            
-        </div>
-    </div>
-  )
+export default class MovDetail extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default MovDetail
